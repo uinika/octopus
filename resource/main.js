246c861e015462abd6995ce7fcaa0448c0f98ee6
hank.zheng <uinika@outlook.com>
2018-11-08 20:19:24 +0800

    Init Project

---
+++ resource/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 500 });
+
+  mainWindow.setMenu(null);
+  mainWindow.loadFile("resource/index.html");
+
+  mainWindow.webContents.openDevTools() // 开启调试模式
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  /* 在Mac系统用户通过Cmd+Q显式退出之前，保持应用程序和菜单栏处于激活状态。*/
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  /* 当dock图标被点击并且不会有其它窗口被打开的时候，在Mac系统上重新建立一个应用内的window。*/
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
