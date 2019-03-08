2f67839b2c71357a080f750c7ee9b4ae09376c86
hank.zheng <uinika@outlook.com>
2019-03-08 18:27:25 +0800

    update

--- resource/main.js
+++ resource/main.js
@@ -3,11 +3,11 @@ const { app, BrowserWindow } = require("electron");
 let mainWindow;
 
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 500, frame: false });
+  mainWindow = new BrowserWindow({ width: 1200, height: 800, frame: true });
 
   mainWindow.setMenu(null);
-  mainWindow.loadFile("resource/index.html");
-  // mainWindow.loadURL("http://localhost:4200/login");
+  // mainWindow.loadFile("resource/index.html");
+  mainWindow.loadURL("http://localhost:4200/login");
 
   mainWindow.webContents.openDevTools(); // 开启调试模式
 
